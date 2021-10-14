0c488e84ac5f4d1cc96dfd3e2e165cf0af74fe4f
Stathis <16375254+nFin1ty@users.noreply.github.com>
2021-10-14 18:28:44 +0300

    Update pfl-bot.js
    - Basic fix for new menu URL.
    - Updates for new discord.js and Discord Intents

--- pfl-bot.js
+++ pfl-bot.js
@@ -1,79 +1,42 @@
 const Discord = require('discord.js');
-const fetch = require('node-fetch')
+const axios = require('axios');
 const TurndownService = require('turndown')
+const cheerio = require('cheerio')
 
-const client = new Discord.Client();
+const { Client, Intents } = require('discord.js');
+const myIntents = new Intents(14077);
+const client = new Client({ intents: myIntents });
 
 var turndownService = new TurndownService()
-turndownService.addRule('p', {
-  filter: ['p'],
-  replacement: function (content) {
-    return '\n' + content
-  }
-})
+		
+		turndownService
+			.addRule('paragraph', {
+				filter: ['p', 'hr'],
+				replacement: (content) => '\n' + content,
+			})
+			.addRule('bullet list', {
+			  filter: ['li'],
+			  replacement: (content) => '\n•' + content,
+			})
+			.addRule('rm-headers', {
+			  filter: ['h3', 'h4'],
+			  replacement: () => ''
+			})
 
-client.on('ready', () => {
-  console.log('I am ready!');
-
-
-client.on('message', message => {
+client.on('messageCreate', message => {
 	
 	if (message.content.toLowerCase() === '!lesxi') {
 		
-		var days = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
-		var days_el = ['Κυριακής', 'Δευτέρας', 'Τρίτης', 'Τετάρτης', 'Πέμπτης', 'Παρασκευής', 'Σαββάτου'];
 		var date = new Date();
-		var dayField = "field_club_menu_" + days[date.getDay()]
-		let url = "https://www.auth.gr/phone_services/node/3280";
-		let settings = { method: "Get" };
- 
-		fetch(url, settings)
-			.then(res => res.json())
-			.then((json) => {
-				menu = (json[dayField]["und"][0]["safe_value"]);
-		 
-				// Remove unwanted and inconsistent HTML elements
-				menu = menu.replace(/<hr \/>/gm, '');	
-				menu = menu.replace(/<h3>/gm, '');
-				menu = menu.replace(/<div>/gm, '');
-				menu = menu.replace(/\t/gm, '');	
-				menu = menu.replace(/ /gm, '');	//nbsp characters
+		today = date.getDay();
+		panetarget = ".kt-accordion-pane-" + today;
 
-		 
-				var menumd = turndownService.turndown(menu);
-
-				// Fix for 'Μεσημεριανό', 'Βραδινό' labels not on newline
-				menumd = menumd.replace(/(\S)(\*\*Μεσημεριανό)/g, "$1\n\n$2");
-				menumd = menumd.replace(/(\S)(\*\*Βραδινό)/g, "$1\n\n$2");
-				
-				//Break menu to embed fields
-				var regexp = /\n([^\*\*]*)/g;
-				var match = regexp.exec(menumd);
-				
-				var fields = [];
-				
-				while (match != null) {
-					if (/\S/g.test(match[1])) {
-						field = match[1].replace(/\n\n/gm, '');
-						fields.push(field);
-					}
-					match = regexp.exec(menumd);
-				}			
-				
-				const menuEmbed = new Discord.RichEmbed()
-					.setColor(0xff0080)
-					.setTitle('Μενoύ ' + days_el[date.getDay()])
-					.setURL('https://www.auth.gr/units/596/weekly-menu')
-					.addField('Πρωινό', fields[0])
-					.addBlankField()
-					.addField('Μεσημεριανό - 1η επιλογή', fields[1], true)
-					.addField('Μεσημεριανό - 2η επιλογή', fields[2], true)
-					.addBlankField()
-					.addField('Βραδινό - 1η επιλογή', fields[3], true)
-					.addField('Βραδινό - 2η επιλογή', fields[4], true)
-				
-				message.channel.send(menuEmbed);
-			});
+		( async () => {
+		  const result = await axios.get('https://www.auth.gr/weekly-menu/');
+		  const $ = cheerio.load(result.data);
+		  var menumd = turndownService.turndown($(panetarget).html());
+		  message.channel.send(menumd);
+		})();
         
 	}
 	
